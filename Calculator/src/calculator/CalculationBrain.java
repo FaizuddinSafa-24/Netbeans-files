c6c7f9ff9ab79a003755b95abf5b33c259a38a0e
Faizuddin Safa <safarajuk19@gmail.com>
2026-03-30 15:41:08 +0600

    edge case: user input only numbers, not intend to operate, run the program instead

--- Calculator/src/calculator/CalculationBrain.java
+++ Calculator/src/calculator/CalculationBrain.java
@@ -8,43 +8,49 @@ package calculator;
  *
  * @author safa
  */
-public class CalculationBrain{
-    private  double currentValue,pastValue;
+public class CalculationBrain {
+
+    private double currentValue, pastValue;
     private Operation pending;
     private String inputNow;
-    
+
     public void inputDigit(String digit) {
-        this.inputNow+= digit;
+        this.inputNow += digit;
     }
+
     public void setOperation(Operation operate) {
-     this.pastValue= Double.parseDouble(inputNow);
-     this.pending= operate;
-     this.inputNow="";
-     
+        this.pastValue = Double.parseDouble(inputNow);
+        this.pending = operate;
+        this.inputNow = "";
+
     }
+
     public double calculate() {
-        this.currentValue= Double.parseDouble(inputNow);
-        if(pending==null) { 
-            throw new CalculatorException("Arit
+        this.currentValue = Double.parseDouble(inputNow);
+        if (pending == null) {
+
         } else {
-            this.currentValue= pending.executing(pastValue, currentValue);
+            this.currentValue = pending.executing(pastValue, currentValue);
             return this.currentValue;
         }
     }
+
     public void clear() {
-        this.currentValue= 0.0;
-        this.pastValue= 0.0;
-        this.inputNow= "";
-        this.pending=null;
+        this.currentValue = 0.0;
+        this.pastValue = 0.0;
+        this.inputNow = "";
+        this.pending = null;
     }
+
     public double getCurrentValue() {
-        
+
     }
+
     public String getInputNow() {
-        
+
     }
+
     public boolean pendingOperation() {
-        
+
     }
 }
- 
