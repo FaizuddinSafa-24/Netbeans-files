675dd2fa00e8f4085764f24b95dcd5f482447e35
Faizuddin Safa <safarajuk19@gmail.com>
2026-03-30 12:59:20 +0600

    CalculationBrain basic UML based design done, clear method written

---
+++ Calculator/src/calculator/CalculationBrain.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculator;
+
+/**
+ *
+ * @author safa
+ */
+public class CalculationBrain{
+    private  double currentValue,pastValue;
+    private Operation pending;
+    private String inputNow;
+    
+    public void inputDigit(String digit) {
+        
+    }
+    public void setOperation(Operation operate) {
+        
+    }
+    public double calculate() {
+        
+    }
+    public void clear() {
+        this.currentValue= 0.0;
+        this.pastValue= 0.0;
+        this.inputNow= "";
+        this.pending=null;
+    }
+    public double getCurrentValue() {
+        
+    }
+    public String getInputNow() {
+        
+    }
+    public boolean pendingOperation() {
+        
+    }
+}
+ 
