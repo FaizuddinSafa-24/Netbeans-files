aa297e782ed216c3566635df5fbb77cba6bc9ff7
Faizuddin Safa <safarajuk19@gmail.com>
2026-04-03 22:13:44 +0600

    added  setTitle(string)

--- GUI/src/gui/JFrame1.java
+++ GUI/src/gui/JFrame1.java
@@ -12,10 +12,11 @@ import javax.swing.JFrame;
  */
 public class JFrame1 {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("Welcome to Sw");
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //frame.setSize(200, 400);
         frame.setBounds(200, 100, 500, 400);
+        frame.setTitle("Welcome");
     }
 }
