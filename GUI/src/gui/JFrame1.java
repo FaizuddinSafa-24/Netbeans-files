8708969da9232f4a90c62560051e0d99704a223d
Faizuddin Safa <safarajuk19@gmail.com>
2026-04-03 22:17:00 +0600

    setResizable turned to false: no more resize

--- GUI/src/gui/JFrame1.java
+++ GUI/src/gui/JFrame1.java
@@ -12,11 +12,12 @@ import javax.swing.JFrame;
  */
 public class JFrame1 {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("Welcome to Swing");
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //frame.setSize(200, 400);
         frame.setBounds(200, 100, 500, 400);
-        frame.setTitle("Welcome");
+        //frame.setTitle("Welcome");
+        frame.setResizable(false);
     }
 }
