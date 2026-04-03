cca0c0a29e66118b9a28adf3df0885ed421271b7
Faizuddin Safa <safarajuk19@gmail.com>
2026-04-03 21:57:32 +0600

    additon input taking show message

---
+++ GUI/src/gui/AddNum.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author safa
+ */
+public class AddNum {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Add Two Numbers:");
+        String firstName = JOptionPane.showInputDialog(frame, "Enter the first number:");
+        String secondName = JOptionPane.showInputDialog(frame, "Enter the second number:");
+        int first = Integer.parseInt(firstName);
+        int second = Integer.parseInt(secondName);
+        int sum = first+second;
+        JOptionPane.showMessageDialog(frame, "The sum is: "+sum);
+        System.exit(0);
+        
+    }
+}
