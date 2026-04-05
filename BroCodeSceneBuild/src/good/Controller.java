0d3b1cd053ab697d5c8d86503943458405e785fa
Faizuddin Safa <safarajuk19@gmail.com>
2026-04-05 19:47:26 +0600

    Event handling: With SceneBuilder, .fxml and controller file worked perfectly, show circle to move

---
+++ BroCodeSceneBuild/src/good/Controller.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package good;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
+
+/**
+ *
+ * @author safa
+ */
+public class Controller {
+    @FXML
+    private Circle circle;
+    private double x,y;
+    public void up(ActionEvent e ){
+        System.out.println("UP");
+        circle.setCenterY(y-=10);
+    }
+    public void down(ActionEvent e ){
+        circle.setCenterY(y+=10);
+        System.out.println("Down");
+    }
+    public void left(ActionEvent e ){
+        System.out.println("Left");
+        circle.setCenterX(x-=10);
+    }
+    public void right(ActionEvent e ){
+        System.out.println("Right");
+        circle.setCenterX(x+=10);
+    }
+}
