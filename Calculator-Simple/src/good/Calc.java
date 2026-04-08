fe6f1045ba37f498f783d181d5ded69065e48bf5
Faizuddin Safa <safarajuk19@gmail.com>
2026-04-08 11:16:47 +0600

    Decimal case considered: working clean

--- Calculator-Simple/src/good/Calc.java
+++ Calculator-Simple/src/good/Calc.java
@@ -24,7 +24,7 @@ public class Calc {
 
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
-            if (Character.isDigit(c)) {
+            if (Character.isDigit(c) || c =='.') {
                 current += c;
             } else {
                 num = Double.parseDouble(current);
