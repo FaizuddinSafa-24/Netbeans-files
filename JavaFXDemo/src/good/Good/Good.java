bb46f1cc53c189b8fc918f5740370a7b26b0cde3
Faizuddin Safa <safarajuk19@gmail.com>
2026-04-08 20:03:35 +0600

    using anonu=ymous class,button working

--- JavaFXDemo/src/good/Good/Good.java
+++ JavaFXDemo/src/good/Good/Good.java
@@ -1,22 +1,33 @@
 package good.Good;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Good extends Application {
+
     public static void main(String[] args) {
         launch(args);
     }
+
     @Override
     public void start(Stage stage) {
         Button button = new Button("Click me");
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("You called button.");
+            }
+
+        }
+        );
         StackPane root = new StackPane();
         root.getChildren().add(button);
-        
-        Scene scene = new Scene(root,600,400);
+        Scene scene = new Scene(root, 600, 400);
         stage.setScene(scene);
         stage.show();
     }
