2926089c96788008a90ba7b40b9df94c259aad2c
Faizuddin Safa <safarajuk19@gmail.com>
2026-04-08 19:53:42 +0600

    program first in javafx course

---
+++ JavaFXDemo/src/good/Good/Good.java
@@ -0,0 +1,23 @@
+package good.Good;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Good extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) {
+        Button button = new Button("Click me");
+        StackPane root = new StackPane();
+        root.getChildren().add(button);
+        
+        Scene scene = new Scene(root,600,400);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
